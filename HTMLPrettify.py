6740747927c0b72b087502848429e2fbb549d8ee
loklaan <bunsiebun@gmail.com>
2012-04-14 00:48:45 +1000

    Correct path to run.js (linux issue?)

--- HTMLPrettify.py
+++ HTMLPrettify.py
@@ -12,7 +12,7 @@ class HtmlprettifyCommand(sublime_plugin.TextCommand):
   def prettify(self, edit):
     regx = re.compile(" ")
     html = commands.getoutput("node " +
-      regx.sub("\ ", sublime.packages_path()) + "/HTMLPrettify/scripts/run.js " +
+      regx.sub("\ ", sublime.packages_path()) + "/Sublime-HTMLPrettify/scripts/run.js " +
       regx.sub("\ ", self.view.file_name()) +
         " indent_size:\ 2" +
         " indent_char:\ ' '" +
