84c955ae56d579055bc0526e1d4d3cf680fa7377
Victor Porof <victor.porof@gmail.com>
2012-04-26 16:30:32 -0400

    Trimmed trailing whitespace and added newline at the end of the file

--- HTMLPrettify.py
+++ HTMLPrettify.py
@@ -10,19 +10,19 @@ class HtmlprettifyCommand(sublime_plugin.TextCommand):
     self.view.run_command("save")
 
   def prettify(self, edit):
-    scriptPath = sublime.packages_path() + "/Sublime-HTMLPrettify/scripts/run.js"    
+    scriptPath = sublime.packages_path() + "/Sublime-HTMLPrettify/scripts/run.js"
     setings = ' '.join([
       "indent_size:\ 2",
       "indent_char:\ ' '",
       "max_char:\ 80",
       "brace_style:\ collapse"
     ])
-    cmd = ["node",scriptPath,self.view.file_name(),setings] 
+    cmd = ["node",scriptPath,self.view.file_name(),setings]
 
-    if sublime.platform()=='windows':  
-      p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)      
-      html = p.communicate()[0]      
-    else:            
+    if sublime.platform()=='windows':
+      p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+      html = p.communicate()[0]
+    else:
       html = commands.getoutput('"'+'" "'.join(cmd)+'"')
 
     if len(html) > 0:
