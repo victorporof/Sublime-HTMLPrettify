0fb2d471a5a43e0e4696e36eca1dcdb36b3578e0
Victor Porof <vporof@mozilla.com>
2013-07-01 00:38:32 +0300

    Rewrite, fixed parsing bugs

--- scripts/run.js
+++ scripts/run.js
@@ -21,65 +21,59 @@
  *    distribution.
  */
 
-/*global console, process, require, __dirname */
-
 (function() {
   "use strict";
 
-  var i, len, hash, key, value, raw,
+  // Cache the console log function and the process arguments.
+  var log = console.log;
+  var argv = process.argv;
 
-    // cache the console log function and the process arguments
-    log = console.log,
-    argv = process.argv,
+  // Require path and file system utilities to load the jshint.js file.
+  var path = require('path');
+  var fs = require('fs');
 
-    // require path and file system utilities to load the jshint.js file
-    path = require('path'),
-    fs = require('fs'),
+  // The source file to be linted and options.
+  var source = argv[2] || "";
+  var settings = (argv[3] || "").split(",");
+  var option = {};
 
-    // the source file to be linted and options
-    source = argv[2] || "",
-    option = {};
+  var html_beautify = require(path.join(__dirname, "beautify-html.js")).html_beautify;
+  var js_beautify = require(path.join(__dirname, "beautify.js")).js_beautify;
+  var css_beautify = require(path.join(__dirname, "beautify-css.js")).css_beautify;
 
-  // the style_html and js_beautify functions work when global by dependance
-  global.style_html =
-    require(path.join(__dirname, "beautify-html.js")).style_html;
-
-  global.js_beautify =
-    require(path.join(__dirname, "beautify.js")).js_beautify;
-
-  global.cssbeautify =
-    require(path.join(__dirname, "cssbeautify.js")).cssbeautify;
-
-  // continue only if the source file is specified
-  if (source !== "") {
-
-    // extra arguments with custom options could be passed, so check them now
-    // and add them to the options object
-    for (i = 3, len = argv.length; i < len; i++) {
-      hash = argv[i].split(": ");
-      key = hash[0];
-      value = hash[1];
-
-      // options are stored in key value pairs, such as option.es5 = true
-      option[key] = value;
-    }
-
-    // read the source file and, when complete, lint the code
-    fs.readFile(source, "utf8", function(err, data) {
-      if (err) {
-        return;
-      }
-
-      // format the code
-      if (source.match(".html?" + "$")) {
-        log(style_html(data, option));
-      }
-      else if (source.match(".css" + "$") == ".css") {
-        log(cssbeautify(data, option));
-      }
-      else if (source.match(".js" + "$") == ".js") {
-        log(js_beautify(data, option));
-      }
-    });
+  // Continue only if the source file is specified.
+  if (source == "") {
+    return;
   }
+
+  // Extra arguments with custom options could be passed, so check them now
+  // and add them to the options object.
+  for (var i = 0, len = settings.length; i < len; i++) {
+    var hash = settings[i].split(":");
+    if (hash.length != 2) {
+      continue;
+    }
+    var key = hash[0].trim();
+    var value = hash[1].replace(/^\ /, "");
+
+    // Options are stored in key value pairs, such as option.indent_size = 2.
+    option[key] = value;
+  }
+
+  // Read the source file and, when complete, beautify the code.
+  fs.readFile(source, "utf8", function(err, data) {
+    if (err) {
+      log("Error, unable to continue.");
+      return;
+    }
+    else if (source.match(".html?" + "$")) {
+      log(html_beautify(data, option));
+    }
+    else if (source.match(".css?" + "$")) {
+      log(css_beautify(data, option));
+    }
+    else if (source.match(".jsm?" + "$")) {
+      log(js_beautify(data, option));
+    }
+  });
 }());
