54562b61266d47400800fb12dd1e0da683aef9f4
Victor Porof <vporof@mozilla.com>
2013-07-03 10:39:35 +0300

    Move utility functions at the bottom of the file

--- HTMLPrettify.py
+++ HTMLPrettify.py
@@ -6,40 +6,9 @@ try:
 except ImportError:
   pass
 
-def exists_in_path(cmd):
-  # Can't search the path if a directory is specified.
-  assert not os.path.dirname(cmd)
-  path = os.environ.get("PATH", "").split(os.pathsep)
-  extensions = os.environ.get("PATHEXT", "").split(os.pathsep)
-
-  # For each directory in PATH, check if it contains the specified binary.
-  for directory in path:
-    base = os.path.join(directory, cmd)
-    options = [base] + [(base + ext) for ext in extensions]
-    for filename in options:
-      if os.path.exists(filename):
-        return True
-
-  return False
-
-def get_output(cmd):
-  if int(sublime.version()) < 3000:
-    if sublime.platform() != "windows":
-      # Handle Linux and OS X in Python 2.
-      run = '"' + '" "'.join(cmd) + '"'
-      return commands.getoutput(run)
-    else:
-      # Handle Windows in Python 2.
-      return subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
-  else:
-    # Handle all OS in Python 3.
-    run = '"' + '" "'.join(cmd) + '"'
-    return subprocess.check_output(run, stderr=subprocess.STDOUT, shell=True)
-
-def open_htmlprettify(window, line):
-  window.open_file(PLUGIN_FOLDER + "/HTMLPrettify.py:" + str(line), sublime.ENCODED_POSITION)
-
 PLUGIN_FOLDER = os.path.dirname(os.path.realpath(__file__))
+SETTINGS_LINE = 17
+NODE_LINE = 38
 
 class HtmlprettifyCommand(sublime_plugin.TextCommand):
   def run(self, edit):
@@ -74,7 +43,7 @@ class HtmlprettifyCommand(sublime_plugin.TextCommand):
     except:
       msg = "Node.js was not found in the default path. Please specify the location."
       if sublime.ok_cancel_dialog(msg):
-        open_htmlprettify(self.view.window(), 36)
+        open_htmlprettify(self.view.window(), NODE_LINE)
       else:
         msg = "You won't be able to use this plugin without specifying the path to Node.js."
         sublime.error_message(msg)
@@ -92,8 +61,41 @@ class HtmlprettifyCommand(sublime_plugin.TextCommand):
 
 class HtmlprettifySetOptionsCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    open_htmlprettify(self.view.window(), 15)
+    open_htmlprettify(self.view.window(), SETTINGS_LINE)
 
 class HtmlprettifySetNodePathCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    open_htmlprettify(self.view.window(), 36)
+    open_htmlprettify(self.view.window(), NODE_LINE)
+
+def exists_in_path(cmd):
+  # Can't search the path if a directory is specified.
+  assert not os.path.dirname(cmd)
+  path = os.environ.get("PATH", "").split(os.pathsep)
+  extensions = os.environ.get("PATHEXT", "").split(os.pathsep)
+
+  # For each directory in PATH, check if it contains the specified binary.
+  for directory in path:
+    base = os.path.join(directory, cmd)
+    options = [base] + [(base + ext) for ext in extensions]
+    for filename in options:
+      if os.path.exists(filename):
+        return True
+
+  return False
+
+def get_output(cmd):
+  if int(sublime.version()) < 3000:
+    if sublime.platform() != "windows":
+      # Handle Linux and OS X in Python 2.
+      run = '"' + '" "'.join(cmd) + '"'
+      return commands.getoutput(run)
+    else:
+      # Handle Windows in Python 2.
+      return subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+  else:
+    # Handle all OS in Python 3.
+    run = '"' + '" "'.join(cmd) + '"'
+    return subprocess.check_output(run, stderr=subprocess.STDOUT, shell=True)
+
+def open_htmlprettify(window, line):
+  window.open_file(PLUGIN_FOLDER + "/HTMLPrettify.py:" + str(line), sublime.ENCODED_POSITION)
