7e0b707e0744ef590b3089934eabb583beeceeb8
Victor Porof <vporof@mozilla.com>
2013-07-05 21:38:08 +0300

    Update node line

--- HTMLPrettify.py
+++ HTMLPrettify.py
@@ -12,7 +12,7 @@ except ImportError:
 
 PLUGIN_FOLDER = os.path.dirname(os.path.realpath(__file__))
 OUTPUT_VALID = b"*** HTMLPrettify output ***"
-NODE_LINE = 33
+NODE_LINE = 46
 
 class HtmlprettifyCommand(sublime_plugin.TextCommand):
   def run(self, edit):
