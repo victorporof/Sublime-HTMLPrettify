54270a3f0d165c42dd1a2462881af4c66a7ce0c2
Victor Porof <vporof@mozilla.com>
2013-07-05 15:56:23 +0300

    Search for .jsbeautifyrc files in the source or home folders as well

--- scripts/run.js
+++ scripts/run.js
@@ -37,6 +37,7 @@
   var filePath = argv[3] || "";
   var options = {};
 
+  // This stuff does all the magic.
   var html_beautify = require(path.join(__dirname, "beautify-html.js")).html_beautify;
   var js_beautify = require(path.join(__dirname, "beautify.js")).js_beautify;
   var css_beautify = require(path.join(__dirname, "beautify-css.js")).css_beautify;
@@ -44,6 +45,9 @@
   // Some handy utility functions.
   function isTrue(value) {
     return value == "true" || value == true;
+  }
+  function getUserHome() {
+    return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
   }
   function getOptions(file) {
     var data = fs.readFileSync(file, "utf8");
@@ -68,13 +72,31 @@
     }
   }
 
-  var jshintrc = ".jsbeautifyrc";
+  var jsbeautifyrc = ".jsbeautifyrc";
   var pluginFolder = __dirname.split(path.sep).slice(0, -1).join(path.sep);
-  var jshintrcPath;
+  var sourceFolder = filePath.split(path.sep).slice(0, -1).join(path.sep);
+  var sourceParent = filePath.split(path.sep).slice(0, -2).join(path.sep);
+  var jsbeautifyrcPath;
 
   // Try and get some persistent options from the plugin folder.
-  if (fs.existsSync(jshintrcPath = pluginFolder + path.sep + jshintrc)) {
-    setOptions(jshintrcPath, options);
+  if (fs.existsSync(jsbeautifyrcPath = pluginFolder + path.sep + jsbeautifyrc)) {
+    setOptions(jsbeautifyrcPath, options);
+  }
+
+  // When a JSBeautify config file exists in the same dir as the source file or
+  // one dir above, then use this configuration to overwrite the default prefs.
+
+  // Try and get more options from the source's folder.
+  if (fs.existsSync(jsbeautifyrcPath = sourceFolder + path.sep + jsbeautifyrc)) {
+    setOptions(jsbeautifyrcPath, options);
+  }
+  // ...or the parent folder.
+  else if (fs.existsSync(jsbeautifyrcPath = sourceParent + path.sep + jsbeautifyrc)) {
+    setOptions(jsbeautifyrcPath, options);
+  }
+  // ...or the user's home folder if everything else fails.
+  else if (fs.existsSync(jsbeautifyrcPath = getUserHome() + path.sep + jsbeautifyrc)) {
+    setOptions(jsbeautifyrcPath, options);
   }
 
   function isHTML(path, data) {
@@ -85,7 +107,9 @@
   }
 
   function isCSS(path, data) {
-    return path.match(/\.css?$/) || path.match(/\.less?$/);
+    return path.match(/\.css?$/) ||
+      path.match(/\.sass?$/) ||
+      path.match(/\.less?$/);
   }
 
   function isJS(path, data) {
