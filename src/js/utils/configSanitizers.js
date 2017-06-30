84cdcfc9eb8333d8cb9ca25b6bcbd4636621e8bb
Victor Porof <victor.porof@gmail.com>
2017-06-30 15:03:52 +0200

    Make mapping of .editorconfig files to .jsbeautifyrc stricter
    Signed-off-by: Victor Porof <victor.porof@gmail.com>

--- src/js/utils/configSanitizers.js
+++ src/js/utils/configSanitizers.js
@@ -3,7 +3,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import pick from 'lodash/pick';
+import isObject from 'lodash/isObject';
+import pickBy from 'lodash/pickBy';
 import mapObj from 'map-obj';
+import isGlob from 'is-glob';
 
 const VALID_JSBEAUTIFY_CONFIG_KEYS = [
   'all',
@@ -64,19 +67,16 @@ export const sanitizeJsbeautifyConfig = jsbeautifyConfig =>
     }
   });
 
-// Maps an .editorconfig object to its respective .jsbeautifyrc form.
-export const translateEditorConfigToJsbeautifyConfig = editorConfig =>
-  mapObj(editorConfig, (globString, globConfig) =>
-    ['custom', {
-      [globString]: mapObj(globConfig, (prefName, prefValue) => {
-        switch (prefName) {
-          case 'indent_style':
-            return ['indent_char', sanitizeCharishValues(prefValue)];
-          case 'insert_final_newline':
-            return ['end_with_newline', prefValue];
-          default:
-            return [prefName, prefValue];
-        }
-      }),
-    }],
-  );
+export const translateEditorConfigToJsbeautifyConfig = editorConfig => ({
+  custom: mapObj(pickBy(editorConfig, (v, k) => isGlob(k) && isObject(v)), (globString, globConfig) =>
+    [globString, mapObj(globConfig, (prefName, prefValue) => {
+      switch (prefName) {
+        case 'indent_style':
+          return ['indent_char', sanitizeCharishValues(prefValue)];
+        case 'insert_final_newline':
+          return ['end_with_newline', prefValue];
+        default:
+          return [prefName, prefValue];
+      }
+    })]),
+});
