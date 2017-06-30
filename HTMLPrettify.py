3a398ba1c5dc949ff447e3a1a2ac4d23dc1d6c83
Victor Porof <victor.porof@gmail.com>
2017-06-30 10:21:57 +0200

    Fix python imports not working in Sublime Text 2
    Signed-off-by: Victor Porof <victor.porof@gmail.com>

--- HTMLPrettify.py
+++ HTMLPrettify.py
@@ -3,11 +3,14 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """HTML Prettify plugin entry point"""
 
+import sys
+from os.path import dirname
 from sublime_plugin import TextCommand
 
-from .src.py.commands import *
-from .src.py.event_listeners import *
-from .src.py.main import main
+sys.path.insert(0, dirname(__file__))
+from src.py.commands import *
+from src.py.event_listeners import *
+from src.py.main import *
 
 
 class HtmlprettifyCommand(TextCommand):
